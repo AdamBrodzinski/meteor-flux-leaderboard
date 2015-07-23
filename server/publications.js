f4a58366eb8904f2a91215a8c3c5f71452157d63
Adam Brodzinski <adambrodzinski@gmail.com>
2015-07-23 11:33:19 -0400

    simulates latency on server publish

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,6 @@
 Meteor.publish('players', function() {
   var cursor = Players.find({}, {fields: {name: 1, score: 1}});
+  // simulate latency to show optimistic UI
+  Meteor._sleepForMs(2000);
   return cursor;
 });
