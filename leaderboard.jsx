0d72c5b3b74d18689313e20d00be8d92133448ff
Adam Brodzinski <adambrodzinski@gmail.com>
2015-07-23 12:00:16 -0400

    adds db fixtures

--- leaderboard.jsx
+++ leaderboard.jsx
@@ -1,14 +1,9 @@
 // Set up a collection to contain player information. On the server,
 // it is backed by a MongoDB collection named "players".
 /*
-Players = new Mongo.Collection("players");
 
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
-    players() {
-      return PlayersDomain.getAll();
-    },
-
     selectedName() {
       return PlayersDomain.getSelectedName();
     },
@@ -26,22 +21,6 @@ if (Meteor.isClient) {
     'click .inc'() {
       var playerId = Session.get("selectedPlayer");
       PlayerActions.incrementScore(playerId, 5);
-    }
-  });
-}
-
-// On server startup, create some players if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      _.each(names, function (name) {
-        Players.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
-        });
-      });
     }
   });
 }
--- server/_startup.js
+++ server/_startup.js
@@ -3,3 +3,18 @@ console.log("Starting Leaderboard Server...");
 
 var Alt = Meteor.npmRequire('alt');
 alt = new Alt();
+
+// run fixtures if db is empty
+Meteor.startup(function () {
+  if (Players.find().count() === 0) {
+    console.log("Running fixtures...");
+    var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+      "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+    names.forEach(function (name) {
+      Players.insert({
+        name: name,
+        score: Math.floor(Random.fraction() * 10) * 5
+      });
+    });
+  }
+});
