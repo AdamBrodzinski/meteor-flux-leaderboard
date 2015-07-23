4095579de2027dc8bdf4f14fa6ed707e6ed54eb0
Adam Brodzinski <adambrodzinski@gmail.com>
2015-07-23 09:55:23 -0400

    adds refresh players action

--- both/actions/player_actions.jsx
+++ both/actions/player_actions.jsx
@@ -10,6 +10,11 @@ class PlayerActions {
     console.log('[PlayerActions] selectPlayer');
     return id;
   }
+
+  refreshPlayersCollection() {
+    console.log('[PlayerActions] refresh data');
+    return {}; // *note* returning fires call to dispatcher
+  }
 }
 
 this.PlayerActions = alt.createActions(PlayerActions);
--- both/stores/player_store.jsx
+++ both/stores/player_store.jsx
@@ -2,15 +2,22 @@
 
 class PlayerStore {
   constructor() {
+    // listen to actions and call store methods when dispatcher emits action
     this.bindListeners({
       onSelectPlayer: PlayerActions.selectPlayer,
       onIncrementScore: PlayerActions.incrementScore,
+      onRefreshPlayersCollection: PlayerActions.refreshPlayersCollection,
     });
 
     this.state = {
       players: [],
       selectedId: ''
     };
+
+    // temp fetch on start
+    Meteor.setTimeout(() => {
+      PlayerActions.refreshPlayersCollection();
+    }, 800);
   }
 
   onIncrementScore(opts) {
@@ -18,12 +25,16 @@ class PlayerStore {
   }
 
   onSelectPlayer(id) {
-    console.log('[PlayerStore] selectPlayer');
     this.setState({selectedId: id});
+    console.log('[PlayerStore] selectPlayer');
   }
 
-  updateTodo(todo) {
-    this.setState({ todos: this.state.todos.concat(todo) });
+  // fetch from Minimongo cache and emit state change
+  onRefreshPlayersCollection() {
+    this.setState({
+      players: Player.findLeaders()
+    });
+    console.log('[PlayerStore] refresh');
   }
 }
 
