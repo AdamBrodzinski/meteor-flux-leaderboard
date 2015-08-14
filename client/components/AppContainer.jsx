8cb705b31ae664f448b27aa64fcb5b3800303a28
Adam Brodzinski <adambrodzinski@gmail.com>
2015-08-14 19:11:16 -0400

    stubs out state so app doesn't crash

--- client/components/AppContainer.jsx
+++ client/components/AppContainer.jsx
@@ -3,23 +3,13 @@
 // for each major component.
 
 AppContainer = React.createClass({
-  // you can also use mixins to eliminate this boilerplate
   getInitialState() {
-    return PlayerStore.getState();
+    return {
+      players: [],
+      selectedId: '',
+      selectedPlayerName: ''
+    }
   },
-
-  componentDidMount() {
-    PlayerStore.listen(this.onChange);
-  },
-
-  componentWillUnmount() {
-    PlayerStore.unlisten(this.onChange);
-  },
-
-  onChange(state) {
-    this.setState(state);
-  },
-
   render() {
     // passes each key of this.state to App as a prop
     return (<App {...this.state} />);
