4a8f06b73aa43c0266710d31ccb9cf1c9831fc7f
Adam Brodzinski <adambrodzinski@gmail.com>
2015-08-14 17:33:49 -0400

    combines reducers in store
    now works in the console!!  try:  store.getState()
    
    actions still won’t work because reducers don’t mutate state (yet)

--- client/store.jsx
+++ client/store.jsx
@@ -1,11 +1,25 @@
 let { createStore, combineReducers } = Redux;
 
-// Redux has a single store. to reduce complexity it allows you to
-// combine several 'reducers' that share this single state object.
+// Redux has a single store. to reduce complexity it allows you to combine
+// several 'reducer' functions that share this single state object.
 // They are combined into one root reducer which is passed to the store
+//
+// the shape of root reducer will then look like:
+//    {
+//      userInterface: {
+//        selectedId: 'ds34sjsa34',
+//        selectedPlayerName: 'Bob Smith'
+//      },
+//      players: [
+//        { mongo doc },
+//        { mongo doc },
+//        { mongo doc }
+//      ]
+//    }
 
 let rootReducer = combineReducers({
-  // TODO create reducers
+  userInterface: Reducers.userInterface,
+  players: Reducers.players,
 });
 
 store = createStore(rootReducer);
