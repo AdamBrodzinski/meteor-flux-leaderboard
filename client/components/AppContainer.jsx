c87273057a79336dd4dc08b780e1c3b1e4607049
Adam Brodzinski <adambrodzinski@gmail.com>
2015-08-14 20:50:47 -0400

    connect to store and pass props to App

--- client/components/AppContainer.jsx
+++ client/components/AppContainer.jsx
@@ -11,7 +11,22 @@ AppContainer = React.createClass({
     }
   },
   render() {
-    // passes each key of this.state to App as a prop
-    return (<App {...this.state} />);
+    //debugger // checkout this.props with debugger!
+    return (<App {...this.props} />);
   }
 });
+
+// choose what state we send to comp. above and it's children, in
+// this app we're sending everything at once, we're also splitting
+// it out into three properties to match previous state shape, you
+// could easily just return `state` for this small app
+
+function mapStateToProps(state) {
+  return {
+    players: state.players,
+    selectedId: state.userInterface.selectedId,
+    selectedPlayerName: state.userInterface.selectedPlayerName
+  };
+}
+
+this.AppContainer = connect(mapStateToProps)(AppContainer);
--- client/lib/app.browserify.js
+++ client/lib/app.browserify.js
@@ -1,3 +1,4 @@
 Redux = require("redux");
 ReactRedux = require("react-redux");
 Provider = ReactRedux.Provider;
+connect = ReactRedux.connect;
